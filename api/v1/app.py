9e51eb08d4b6d0880fab6741535b88454b482b6a
n-elmasry <almassrynada411@gmail.com>
2024-05-26 02:29:55 +0500

    fixing task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,16 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask import CORS
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+CORS(app, origins="0.0.0.0")
+host_var = getenv('HBNB_API_HOST')
+port_var = getenv('HBNB_API_PORT')
 
 
 @app.teardown_appcontext
@@ -16,4 +22,4 @@ def teardown_appcontext(self):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host=host_var, port=port_var, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def show_status():
     """shows api status"""
     return jsonify(status='OK')
