c4907f1ccbec4e1a4e39a8a303f9ed56f16e7ccd
n-elmasry <almassrynada411@gmail.com>
2024-05-26 02:23:25 +0500

    fixing task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,4 +7,4 @@ from flask import jsonify
 @app_views.route('/status', strict_slashes=False)
 def show_status():
     """shows api status"""
-    return jsonify({"status": "OK"})
+    return jsonify(status='OK')
