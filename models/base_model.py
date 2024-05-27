ebd7c71c2b66c9cbc7adc11c7bf2ee9548df8ff8
n-elmasry <almassrynada411@gmail.com>
2024-05-27 16:39:32 +0500

    updating basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -62,11 +62,9 @@ class BaseModel:
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(
-                "%Y-%m-%dT%H:%M:%S")
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(
-                "%Y-%m-%dT%H:%M:%S")
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
